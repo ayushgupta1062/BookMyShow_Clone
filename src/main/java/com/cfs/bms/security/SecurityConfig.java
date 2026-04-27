5d177c9083804fed852fc962296203461d608bc6
Ayush Gupta <ayush.gupta1062@gmail.com>
2026-04-27 15:48:38 +0530

    Update SecurityConfig.java

--- src/main/java/com/cfs/bms/security/SecurityConfig.java
+++ src/main/java/com/cfs/bms/security/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "book-my-show-clone-neon.vercel.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "https://book-my-show-clone-git-main-ayush-guptas-projects-922b609c.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
