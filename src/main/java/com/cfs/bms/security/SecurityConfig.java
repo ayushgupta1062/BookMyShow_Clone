ee7e6a22bed5d52d6148368021a27bc732171ca6
Ayush Gupta <ayush.gupta1062@gmail.com>
2026-04-27 16:07:16 +0530

    Update SecurityConfig.java

--- src/main/java/com/cfs/bms/security/SecurityConfig.java
+++ src/main/java/com/cfs/bms/security/SecurityConfig.java
@@ -55,8 +55,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "https://book-my-show-clone-git-main-ayush-guptas-projects-922b609c.vercel.app"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+configuration.setAllowedOriginPatterns(List.of(
+    "http://localhost:*",
+    "https://*.vercel.app"
+));        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
